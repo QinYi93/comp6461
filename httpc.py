f4ee681095ac5817e1bbf485657d2ef6a7d8fd75
QinYi93 <y_qin201@encs.concordia.ca>
2017-09-25 00:09:44 -0400

    finish -o

--- httpc.py
+++ httpc.py
@@ -1,6 +1,6 @@
 #   python httpc.py get -v 'http://httpbin.org/get?course=networking&assignment=1'
-#   python httpc.py post -v -head Accept:application/json -d '{"test":"test"}' 'http://httpbin.org/post'
-#   python httpc.py post -v -head Accept:application/json -f 'test.json' 'http://httpbin.org/post'
+#   python httpc.py post -v -head Accept:application/json -d '{"test":"test"}' -o 'output.txt' 'http://httpbin.org/post'
+#   python httpc.py post -v -head Accept:application/json -f 'test.json' -o output.txt 'http://httpbin.org/post'
 import sys
 from socket import *
 import json
@@ -45,6 +45,7 @@ subparsers = parser.add_subparsers(help='commands')
 get_parser = subparsers.add_parser('get', help='executes a HTTP GET request and prints the response.')
 get_parser.add_argument("-v", action='store_true', dest="verbose", default=False, help="Prints the detail of the response such as protocol, status, and headers.")
 get_parser.add_argument("-head", action="append", dest="headers", default=[], help="Associates headers to HTTP Request with the format 'key:value'.")
+get_parser.add_argument("-o", action="store", dest="output" , default = "", help = "Output the body to a file", required = False)
 get_parser.set_defaults(which='get')
 
 post_parser = subparsers.add_parser('post', help='executes a HTTP POST request and prints the response.')
@@ -53,6 +54,7 @@ post_parser.add_argument("-head", action="append", dest="headers", default=[], h
 group = post_parser.add_mutually_exclusive_group(required=False)
 group.add_argument("-d", action="store", dest="data", type=json.loads, default='{}', help="Associates an inline data to the body HTTP POST request.")
 group.add_argument("-f", action="store", dest="file", default="", help="Associates the content of a file to the body HTTP POST request.")
+post_parser.add_argument("-o", action="store", dest="output" , default = "", help = "Output the body to a file", required = False)
 post_parser.set_defaults(which='post')
 
 help_parser = subparsers.add_parser('help', help='prints this screen.')
@@ -65,5 +67,9 @@ args = parser.parse_args()
 # parser.print_help()
 h = create_http()
 reply = h.send()
+if args.output:
+    o = open(args.output, 'w')
+    o.write(reply.split("\r\n\r\n",1)[1])
+    o.close()
 if h.getVerbosity():
     print("Reply:\n" + reply)
